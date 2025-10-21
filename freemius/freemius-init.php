f55c89847de9705ee97d278e6b1d5c53d1d3a255
thepluginworks <stuart@thepluginworks.com>
2025-10-21 11:38:07 +0100

    Freemius: pass plugin version (1.0.0) to SDK init

--- freemius/freemius-init.php
+++ freemius/freemius-init.php
@@ -14,6 +14,7 @@ if ( ! function_exists( 'tpw_core_fs' ) ) {
                 'slug'                => 'tpw-core',
                 'type'                => 'plugin',
                 'public_key'          => 'pk_230160ce6b3f69b3aac273acd5aaf',
+                'version'             => defined('TPW_CORE_VERSION') ? TPW_CORE_VERSION : '1.0.0',
                 'is_premium'          => false,
                 'has_addons'          => false,
                 'has_paid_plans'      => true,
